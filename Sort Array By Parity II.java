c5d5952d026ffccf0f2c27bb16041c2821f82436
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-09-29 00:16:47 +0530

    Add files via upload

---
+++ Sort Array By Parity II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int[] result = new int[nums.length];
+        int even=0;
+        int odd=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                result[even]=nums[i];
+                even+=2;
+            }else{
+                result[odd]=nums[i];
+                odd+=2;
+            }
+        }
+        return result;
+    }
+}
